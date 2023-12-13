bf84fee34fd801b5af6bf71b51e53de121dbfac9
Jason A <42155399+nutbarski@users.noreply.github.com>
2023-12-13 10:14:10 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-;Lock;lk
+//grab elements
+
+const usernameInput = document.querySelector('input[name="username"]');
+
+//add event listeners 
+
+usernameInput.addEventListener("submit", validate)
+
